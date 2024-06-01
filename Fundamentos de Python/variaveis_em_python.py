f3b56f3cdfd3369bd487d5adc035a8aaa124ea02
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-01 16:09:57 -0300

    Variáveis em python

---
+++ Fundamentos de Python/variaveis_em_python.py
@@ -0,0 +1,12 @@
+print('Utilizando variáveis:')
+print('Código dificil de entender:')
+resultado1 = 3.14 * 5 ** 2
+print('3.14 * 5 ** 2 = ', resultado1)
+
+print('\nMesmo cálculo utilizando variáveis:')
+pi = 3.14
+raio = 5
+raio_ao_quadrado = raio ** 2
+
+resultado2 = pi * raio_ao_quadrado
+print('pi * raio_ao_quadrado = ', resultado2)
