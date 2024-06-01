c5ae90313343a162eb0bc2a4ae05fceaa6857612
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-01 14:42:24 -0300

    Numeros, tipos de dados e operações matemáticas.

---
+++ Fundamentos de Python/numeros_em_python.py
@@ -0,0 +1,18 @@
+inteiro = 10
+decimal = 5.75
+texto = 'um texto qualquer'
+boleano = True
+print()
+print('Tipos de dados')
+print(inteiro, type(inteiro))
+print(decimal, type(decimal))
+print(texto, type(texto))
+print(boleano, type(boleano))
+print()
+print('Operações matemáticas')
+print('<Soma> 10 + 8 = ' , (10 + 8))
+print('<Subtração> 207 - 42 = ' , (207 - 42))
+print('<Divisão> 256 + 8 = ' , (256 / 8))
+print('<Multiplicação> 55 x 12 = ' , (55 * 12))
+print('<Exponenciação> 5 ** 2 = ' , (5 ** 2))
+
