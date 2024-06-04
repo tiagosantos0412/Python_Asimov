f4df8ebdb0fd1bd56f34305ec63558c66b37a69d
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-03 22:57:39 -0300

    Dados mutáveis e tuplas

---
+++ Modulo 3 - Seuqências e Loops/tuplas_e_dados_mutaveis.py
@@ -0,0 +1,13 @@
+# dados mutáveis
+
+discipulos = ['Pedro', 'Tiago', 'João', 'Mateus']
+
+discipulos[0] = 'Simão'
+discipulos.append('Judas')
+print(discipulos)
+del discipulos[4]
+print(discipulos)
+
+# Tuplas
+valores = (1,2,3)
+print(valores, type(valores))
