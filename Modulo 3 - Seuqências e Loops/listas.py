9a48f5aba3ccd6be75e23b8e048878d471b8c61b
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-03 22:48:05 -0300

    Listas em python.

---
+++ Modulo 3 - Seuqências e Loops/listas.py
@@ -0,0 +1,16 @@
+# Tipo de dado (lista)
+
+lista1 = [1, 2, 3]
+print(lista1, type(lista1))
+
+lista2 = [10, 25.5, True, 'Python']
+print(lista2, type(lista2))
+print(lista2, type(lista2[1]))
+
+lista3 = [7, 5.5, False, 'Java', [5, 7, 'javascript']]
+print(lista3, type(lista3))
+print(lista3, type(lista3[4][1]))
+print(lista3[4][1])
+
+discipulos = ['Pedro', 'Tiago', 'João']
+print(discipulos[-2])
