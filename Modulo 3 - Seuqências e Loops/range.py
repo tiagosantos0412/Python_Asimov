dd91006c3a7b38056ce7555d21c5457a48c93b20
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-14 10:00:07 -0300

    Ranges.

---
+++ Modulo 3 - Seuqências e Loops/range.py
@@ -0,0 +1,16 @@
+print(range(10))
+
+# Atribui o range de 1 a 10 na variável números convertendo-os em uma lista de 0 a 9
+numeros = list(range(10))
+print(numeros)
+
+# Utilizando slice
+print(numeros[1:5])
+
+# Utilizando range
+print(list(range(1,5)))
+
+# Retona de 0 a 10 pulando de 2 em 2
+print(numeros[0:10:2])
+# Retona de 0 a 10 pulando de 2 em 2 utilizando range
+print(list(range(0, 10, 2)))
