f114fedc816e7aeee3cd4d52c20bad981130dee4
Tiago Felipe <tiagosantos0412@gmail.com>
2024-06-14 10:17:09 -0300

    For loops.

---
+++ Modulo 3 - Seuqências e Loops/for_loops.py
@@ -0,0 +1,13 @@
+# Montando uma estrutura de repetição de 0 a 9
+for i in range(10):
+    print(f'O índice agora vale {i}')
+
+# Montando uma estrutura de repetição de 1 a 10
+for i in range(1,11):
+    print(f'O índice agora vale {i}')
+
+# Tabuada
+numero = 8
+for i in range (1,11):
+    print(f'{i} X {numero} = {numero * i}')
+
